89cbacf3b53e7974284841a44aaca93b4a3e6fc9
Allan Latty <allan.latty@gmail.com>
2022-10-25 14:23:38 -0400

    Established a server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+app.use (
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+
+app.use(bodyParser.json())
+
+//DB Configuration 
+const db = require("./config/keys").mongoURI
+
+//Connect to MongoDB
+mongoose.connect(
+    db, {useNewURLParser: true}
+)
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch(err => console.log(err))
+
+
+//Assign port number from .env file
+const PORT = process.env.PORT
+
+//Start server and return response
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
