d34bb6fed58d856392e88c66abf5b8ec83e6f48d
Allan Latty <allan.latty@gmail.com>
2022-10-25 16:00:44 -0400

    Fixed router bug

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const router = require("router")
+const router = express.Router()
 const bcrypt = require("bcryptjs")
 const jwt  = require("jsonwebtoken")
 const keys = require("../../config/keys")
